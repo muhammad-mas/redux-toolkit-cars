821bb90531ee886514d6374355821dff87f204d0
Muhammad Arslan <muhammad.arslan@greybeardsupport.com>
2024-01-10 11:26:36 +0500

    Added Search , Highlighting

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -2,11 +2,19 @@ import { useDispatch, useSelector } from "react-redux";
 import { removeCar } from "../store/store";
 
 function CarList() {
-  const { data, searchTerm } = useSelector((state) => state.cars);
+  //Derieved state , we are using useSelector to get the state from the store
+  const [data, { name, cost }] = useSelector(
+    ({ cars: { data, searchTerm }, form }) => {
+      return [
+        data.filter((car) =>
+          car.name.toLowerCase().includes(searchTerm.toLowerCase())
+        ),
+        form,
+      ];
+    }
+  );
   const dispatch = useDispatch();
-  //   if (searchTerm === "") {
-  //     return data.map((car) => <p key={car.id}>{car.name}</p>);
-  //   }
+
   //   const filteredCars = data.filter((car) =>
   //     car.name.toLowerCase().includes(searchTerm.toLowerCase())
   //   );
@@ -15,8 +23,10 @@ function CarList() {
     dispatch(removeCar(id));
   };
   const renderedCars = data.map((car) => {
+    const bold = name && car.name.toLowerCase().includes(name.toLowerCase());
+
     return (
-      <div key={car.id} className="car-form panel">
+      <div key={car.id} className={`panel ${bold && "bold"}`}>
         <p>
           {car.name} - ${car.cost}
         </p>
--- src/components/CarSearch.js
+++ src/components/CarSearch.js
@@ -1,10 +1,23 @@
+import { useDispatch, useSelector } from "react-redux";
+import { updateSearchTerm } from "../store/store";
 function CarSearch() {
+  const { searchTerm } = useSelector((state) => state.cars);
+  const dispatch = useDispatch();
+  const handleSearchTermChange = (e) => {
+    dispatch(updateSearchTerm(e.target.value));
+  };
   return (
     <div className="list-header">
       <h3 className="title is-3">Search</h3>
       <div className="field">
         <div className="control">
-          <input className="input" type="text" placeholder="Search..." />
+          <input
+            className="input"
+            type="text"
+            placeholder="Search..."
+            value={searchTerm}
+            onChange={handleSearchTermChange}
+          />
         </div>
       </div>
     </div>
